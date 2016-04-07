21a72df61321baf864d99084753c7f1bd23ae346
Cedulio Cezar <ceduliocezar@iMac-Produto.bnu.teclogica>
2016-04-07 10:28:15 -0300

    button

---
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Cedulio Cezar on 07/04/16.
+//  Copyright © 2016 Cedulio Cezar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func add(){
+        print("button pressed")
+    }
+
+}
+
