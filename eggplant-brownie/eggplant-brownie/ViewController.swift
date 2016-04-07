118085420eb2135c7d63e9eafd822e419eae3972
Cedulio Cezar <ceduliocezar@iMac-Produto.bnu.teclogica>
2016-04-07 11:08:04 -0300

    chapter 2

--- eggplant-brownie/eggplant-brownie/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var nameField : UITextField!
+    @IBOutlet var hapinessField : UITextField!
+    
     @IBAction func add(){
-        print("button pressed")
+        
+        
+        let name : String = nameField.text!
+        let hapiness : String = hapinessField.text!
+        
+        print("eaten: \(name) \(hapiness)!")
+        
+        
     }
 
 }
