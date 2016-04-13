9c97b882d31133a63ccc4e6d2a5d3c740b97633d
Cedulio Cezar <ceduliocezar@gmail.com>
2016-04-13 09:30:23 -0300

    chapter12, AlertController, alert buttons, Long Press

--- eggplant-brownie/eggplant-brownie/viewcontrollers/MealsTableViewController.swift
+++ eggplant-brownie/eggplant-brownie/viewcontrollers/MealsTableViewController.swift
@@ -27,6 +27,9 @@ class MealsTableViewController: UITableViewController, AddMealDelegate {
         
         cell.textLabel?.text = meal.name
         
+        let longPress = UILongPressGestureRecognizer(target: self, action: Selector("showDetails:"))
+        cell.addGestureRecognizer(longPress)
+        
         return cell
     }
     
@@ -43,5 +46,31 @@ class MealsTableViewController: UITableViewController, AddMealDelegate {
         }
     }
     
+    
+    func showDetails(recognizer: UILongPressGestureRecognizer){
+        if recognizer.state == UIGestureRecognizerState.Began{
+            
+            let cell = recognizer.view as! UITableViewCell
+            
+            let indexPath = tableView.indexPathForCell(cell)
+            
+            if indexPath == nil{
+                return
+            }
+            
+            let row = indexPath?.row
+            
+            
+            let meal =  meals[row!]
+            
+            let details = UIAlertController(title: meal.name, message: meal.showDetails(), preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let ok = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Cancel, handler: nil)
+            
+            details.addAction(ok)
+            
+            presentViewController(details, animated: true, completion: nil)
+        }
+    }
 
 }
